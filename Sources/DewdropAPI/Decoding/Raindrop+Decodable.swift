bbfcbba3011c32d0336aa9edbf397e9b70066bf5
Jordan Kay <jordan@fleuronic.com>
2024-03-21 08:59:22 -0400

    Add decodable support to raindrop

--- Sources/DewdropAPI/Decoding/Raindrop+Decodable.swift
+++ Sources/DewdropAPI/Decoding/Raindrop+Decodable.swift
@@ -4,13 +4,38 @@ extension Raindrop: Decodable {
 	// MARK: Decodable
 	public init(from decoder: Decoder) throws {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
-		fatalError()
+
+		self.init(
+			url: .init(string: try container.decode(String.self, forKey: .url))!,
+			title: try container.decode(String.self, forKey: .title),
+			excerpt: try container.decode(String.self, forKey: .excerpt),
+			domain: try container.decode(String.self, forKey: .domain),
+			coverURL: .init(string: try container.decode(String.self, forKey: .coverURL))!,
+			note: try container.decode(String.self, forKey: .note),
+			tags: try container.decode([String].self, forKey: .tags),
+			isFavorite: try container.decode(Bool.self, forKey: .isFavorite),
+			isBroken: try container.decode(Bool.self, forKey: .isBroken),
+			creationDate: .init(),
+			updateDate: .init()
+		)
 	}
 }
 
 // MARK -
 private extension Raindrop {
 	enum CodingKeys: String, CodingKey {
+		case url = "link"
+		case title
+		// case itemType = "type"
+		case excerpt
 		case domain
+		case coverURL = "cover"
+		case note
+		case tags
+		// case cache
+		case isFavorite = "important"
+		case isBroken = "broken"
+		case creationDate = "created"
+		case updateDate = "lastUpdate"
 	}
 }
