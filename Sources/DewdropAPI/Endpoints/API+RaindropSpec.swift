387b512588908b39f1f023ec5fc0351deca6ceef
Jordan Kay <jordan@fleuronic.com>
2024-03-22 19:08:11 -0400

    Update raindrop spec

--- Sources/DewdropAPI/Endpoints/API+RaindropSpec.swift
+++ Sources/DewdropAPI/Endpoints/API+RaindropSpec.swift
@@ -1,11 +1,12 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import struct Dewdrop.Raindrop
+import struct DewdropService.RaindropFields
 import protocol DewdropService.RaindropSpec
 import protocol Catenary.API
 
 extension API: RaindropSpec {
-	public func fetchRaindrop(with id: Int) async -> Self.Result<Raindrop> {
+	public func fetchRaindrop(with id: Raindrop.ID) async -> Self.Result<RaindropFields> {
 		let path = "raindrop/\(id)"
 		return await getResource(at: path)
 	}
