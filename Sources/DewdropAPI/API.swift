c369a5e64d9179272b434f92d0ff144f2372518d
Jordan Kay <jordan@fleuronic.com>
2024-03-21 22:56:34 -0400

    Add date formatter

--- Sources/DewdropAPI/API.swift
+++ Sources/DewdropAPI/API.swift
@@ -1,19 +1,41 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
-import InitMacro
 import URL
 
 import enum Catenary.Request
 import struct Foundation.URL
+import struct Foundation.Date
+import class Foundation.JSONDecoder
+import class Foundation.ISO8601DateFormatter
 import protocol Catenary.RESTAPI
 
-@Init public struct API {
+public struct API {
 	private let apiKey: String
+	private let dateFormatter: ISO8601DateFormatter
+
+	public init(apiKey: String) {
+		self.apiKey = apiKey
+
+		dateFormatter = .init()
+		dateFormatter.formatOptions = [.withFractionalSeconds, .withInternetDateTime]
+	}
 }
 
 // MARK: -
 extension API: RESTAPI {
 	// MARK: API
+	public var decoder: JSONDecoder {
+		let decoder = JSONDecoder()
+		decoder.keyDecodingStrategy = .convertFromSnakeCase
+		decoder.dateDecodingStrategy = .custom { decoder -> Date in
+			let container = try decoder.singleValueContainer()
+			let dateString = try container.decode(String.self)
+			return dateFormatter.date(from: dateString)!
+		}
+		
+		return decoder
+	}
+
 	public var authenticationHeader: Request.Header? {
 		.init(
 			field: "Authorization",
