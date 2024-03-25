2e2ef7ad4e3b12bc7f8d9c7cb605093c20e757f1
Jordan Kay <jordan@fleuronic.com>
2024-03-25 17:54:46 -0400

    Fix structure

--- Sources/DewdropAPI/Decoding/Collection/Collection+Decodable.swift
+++ Sources/DewdropAPI/Decoding/Collection/Collection+Decodable.swift
@@ -2,24 +2,24 @@
 
 import struct Dewdrop.Collection
 
-extension Collection: Decodable {
-	// MARK: Decodable
-	public init(from decoder: Decoder) throws {
-		let container = try decoder.container(keyedBy: CodingKeys.self)
-		
-		self.init(
-			title: try container.decode(String.self, forKey: .title),
-			count: try container.decode(Int.self, forKey: .count),
-			colorString: try container.decode(String.self, forKey: .colorString)
-		)
-	}
-}
-
-// MARK -
-private extension Collection {
-	enum CodingKeys: String, CodingKey {
-		case title
-		case count
-		case colorString = "color"
-	}
-}
+//extension Collection: Decodable {
+//	// MARK: Decodable
+//	public init(from decoder: Decoder) throws {
+//		let container = try decoder.container(keyedBy: CodingKeys.self)
+//		
+//		self.init(
+//			title: try container.decode(String.self, forKey: .title),
+//			count: try container.decode(Int.self, forKey: .count),
+//			colorString: try container.decode(String.self, forKey: .colorString)
+//		)
+//	}
+//}
+//
+//// MARK -
+//private extension Collection {
+//	enum CodingKeys: String, CodingKey {
+//		case title
+//		case count
+//		case colorString = "color"
+//	}
+//}
--- Sources/DewdropAPI/Endpoints/API+RaindropSpec.swift
+++ Sources/DewdropAPI/Endpoints/API+RaindropSpec.swift
@@ -4,7 +4,7 @@ import struct Dewdrop.Raindrop
 import struct DewdropService.RaindropDetailsFields
 import struct DewdropService.RaindropHighlightListFields
 import protocol DewdropService.RaindropSpec
-import protocol Catenary.API
+//import protocol Catenary.API
 
 extension API: RaindropSpec {
 	public func fetchRaindropDetails(with id: Raindrop.ID) async -> Self.Result<RaindropDetailsFields> {
@@ -15,4 +15,8 @@ extension API: RaindropSpec {
 	public func fetchRaindropHighlights(with id: Raindrop.ID) async -> Self.Result<RaindropHighlightListFields> {
 		await fetchRaindropDetails(with: id).map(RaindropHighlightListFields.init)
 	}
+}
+
+public extension API {
+
 }
