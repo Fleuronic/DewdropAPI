a8da2ec80c006acb99987d6ebbf61151528c440c
Jordan Kay <jordan@fleuronic.com>
2024-06-06 18:37:05 -0400

    Update parameter name

--- Sources/DewdropAPI/API.swift
+++ Sources/DewdropAPI/API.swift
@@ -1,6 +1,7 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import class PapyrusCore.Provider
+import struct DewdropRESTAPI.CollectionEndpointsAPI
 import struct DewdropRESTAPI.BackupEndpointsAPI
 import struct DewdropRESTAPI.ImportEndpointsAPI
 import struct DewdropRESTAPI.TagEndpointsAPI
@@ -10,6 +11,7 @@ import struct DewdropService.ImportFolderFields
 import protocol DewdropService.ImportFields
 
 public struct API<FileImportFields: ImportFields> {
+	let collections: CollectionEndpointsAPI
 	let backups: BackupEndpointsAPI
 	let `import`: ImportEndpointsAPI
 	let tags: TagEndpointsAPI
@@ -21,7 +23,7 @@ public struct API<FileImportFields: ImportFields> {
 public extension API {
 	init(
 		apiKey: String,
-		importFileFields: FileImportFields.Type = ImportFolderFields.self
+		fileImportFields: FileImportFields.Type = ImportFolderFields.self
 	) {
 		let url = "https://api.raindrop.io/rest/v1"
 		let provider = Provider(baseURL: url).modifyRequests { request in
@@ -32,10 +34,11 @@ public extension API {
 			return response
 		}
 
-		backups = BackupEndpointsAPI(provider: provider)
-		`import` = ImportEndpointsAPI(provider: provider)
-		tags = TagEndpointsAPI(provider: provider)
-		filters = FilterEndpointsAPI(provider: provider)
-		users = UserEndpointsAPI(provider: provider)
+		collections = .init(provider: provider)
+		backups = .init(provider: provider)
+		`import` = .init(provider: provider)
+		tags = .init(provider: provider)
+		filters = .init(provider: provider)
+		users = .init(provider: provider)
 	}
 }
