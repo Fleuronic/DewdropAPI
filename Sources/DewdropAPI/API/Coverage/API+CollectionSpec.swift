3a9ce4e52754ca22e189fadcc629aaba9a1d5822
Jordan Kay <jordan@fleuronic.com>
2024-07-10 14:54:58 -0400

    Update spec conformance

--- Sources/DewdropAPI/API/Coverage/API+CollectionSpec.swift
+++ Sources/DewdropAPI/API/Coverage/API+CollectionSpec.swift
@@ -7,22 +7,26 @@ import protocol Catena.Scoped
 import protocol Catenary.API
 
 extension API: CollectionSpec {
+	public typealias CollectionList = Self.Result<[CollectionListFields]>
 	public typealias CollectionListFields = CollectionDetailsFields
+	public typealias ChildCollectionList = CollectionList
+	public typealias ChildCollectionListFields = CollectionListFields
+	public typealias SystemCollectionList = Self.Result<[SystemCollectionListFields]>
 	public typealias SystemCollectionListFields = CollectionCountFields
 
-	public func listRootCollections() async -> Self.Result<[CollectionListFields]> {
+	public func listRootCollections() async -> CollectionList {
 		await result {
 			try await collections.getRootCollections().items
 		}
 	}
 
-	public func listChildCollections() async -> Self.Result<[CollectionListFields]> {
+	public func listChildCollections() async -> ChildCollectionList {
 		await result {
 			try await collections.getChildCollections().items
 		}
 	}
 
-	public func listSystemCollections() async -> Self.Result<[SystemCollectionListFields]> {
+	public func listSystemCollections() async -> SystemCollectionList {
 		await result {
 			try await collections.getSystemCollections().items
 		}
