8760fb55e44adab9374f314bab3e661e88528916
Jordan Kay <jordan@fleuronic.com>
2024-08-19 11:41:55 -0400

    Conform API to highlight spec

---
+++ Sources/DewdropAPI/API/Coverage/API+HighlightSpec.swift
@@ -0,0 +1,23 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+import struct Dewdrop.Raindrop
+import struct DewdropService.HighlightDetailsFields
+import protocol DewdropService.HighlightSpec
+import protocol Catena.Scoped
+import protocol Catenary.API
+
+extension API: HighlightSpec {
+	public typealias HighlightListResult = Self.Result<HighlightDetailsFields>
+
+	public func listHighlights(onPage page: Int?, listing highlightsPerPage: Int?) async -> Self.Result<HighlightDetailsFields> {
+		await result {
+			fatalError()
+		}
+	}
+
+	public func listHighlights(ofRaindropWith id: Raindrop.ID) async -> Self.Result<HighlightDetailsFields> {
+		await result {
+			fatalError()
+		}
+	}
+}
