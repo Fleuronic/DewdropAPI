04389889d8706fdd460c5ff08fe6eede3e21d999
Jordan Kay <jordan@fleuronic.com>
2024-08-19 11:42:06 -0400

    Add highlight endpoints to REST API

---
+++ Sources/DewdropRESTAPI/Endpoints/HighlightEndpoints.swift
@@ -0,0 +1,11 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+import Papyrus
+
+import struct Dewdrop.Highlight
+import struct DewdropService.IdentifiedCollection
+
+@API @JSON(decoder: .dewdrop)
+public protocol HighlightEndpoints {
+
+}
