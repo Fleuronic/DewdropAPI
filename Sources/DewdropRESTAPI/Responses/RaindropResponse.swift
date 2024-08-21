10ebbb6146001dc2337bb1085101558363107c57
Jordan Kay <jordan@fleuronic.com>
2024-08-21 16:06:33 -0400

    Remove unused response

--- Sources/DewdropRESTAPI/Responses/RaindropResponse.swift
+++
@@ -1,6 +0,0 @@
-// Copyright © Fleuronic LLC. All rights reserved.
-
-import struct DewdropService.RaindropDetailsFields
-import protocol Identity.Identifiable
-
-public typealias RaindropsResponse = ItemListResponse<RaindropDetailsFields>
